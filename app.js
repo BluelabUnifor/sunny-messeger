e78075962a8777348c655f4ad2a14c2eb1d3bdc6
Marcelo Gondim <mgmarques94@gmail.com>
2017-04-17 17:27:00 -0300

    facebook-massager

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(bodyParser.json());
 require('./rotas/conversation')(app);
 require('./rotas/speech-to-text')(app);
 require('./rotas/text-to-speech')(app);
+require('./rotas/facebook')
 
 
 module.exports = app;
